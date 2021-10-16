ec997172821c86ee8e5f9e56f3bccc5f6ac0a456
James Healy <james@yob.id.au>
2021-10-16 11:27:40 +1100

    daikin: failures in initial connectivity check shouldn't be fatal
    Occasionally on boot the daikin adapter might fail to connect to a
    device. Like if the network is still booting and this program has come
    up before the AC units have reconnected to the wifi.
    
    Those failures aren't permanent. We should slowly loop around and try
    again.

--- adapters/daikin/daikin.go
+++ adapters/daikin/daikin.go
@@ -44,23 +44,26 @@ func Init(bus *pubsub.Pubsub, logger *logging.Logger, state homestate.StateReade
 }
 
 func broadcastState(bus *pubsub.Pubsub, logger *logging.Logger, config configData) {
-	d, err := daikinClient.NewNetwork(daikinClient.AddressTokenOption(config.address, config.token))
-	if err != nil {
-		logger.Fatal(fmt.Sprintf("daikin (%s): %v", config.name, err))
-		return
-	}
 	insideTempSensor := entities.NewSensorGauge(bus, fmt.Sprintf("daikin.%s.temp_inside_celcius", config.name))
 	outsideTempSensor := entities.NewSensorGauge(bus, fmt.Sprintf("daikin.%s.temp_outside_celcius", config.name))
 	powerSensor := entities.NewSensorBoolean(bus, fmt.Sprintf("daikin.%s.power", config.name))
 	wattHoursTodaySensor := entities.NewSensorGauge(bus, fmt.Sprintf("daikin.%s.watt_hours_today", config.name))
 
-	dev := d.Devices[config.address]
-	if err := dev.GetControlInfo(); err != nil {
-		logger.Fatal(fmt.Sprintf("daikin (%s): %v", config.name, err))
-		return
-	}
-
 	for {
+		time.Sleep(20 * time.Second)
+
+		d, err := daikinClient.NewNetwork(daikinClient.AddressTokenOption(config.address, config.token))
+		if err != nil {
+			logger.Error(fmt.Sprintf("daikin (%s): %v", config.name, err))
+			continue
+		}
+
+		dev := d.Devices[config.address]
+		if err := dev.GetControlInfo(); err != nil {
+			logger.Error(fmt.Sprintf("daikin (%s): %v", config.name, err))
+			continue
+		}
+
 		if err := dev.GetSensorInfo(); err != nil {
 			logger.Error(fmt.Sprintf("daikin (%s): %v", config.name, err))
 			continue
@@ -82,54 +85,56 @@ func broadcastState(bus *pubsub.Pubsub, logger *logging.Logger, config configDat
 		}
 
 		wattHoursTodaySensor.Update(float64(dev.WeekPower.TodayWattHours))
-
-		time.Sleep(20 * time.Second)
 	}
 }
 
 func changeState(bus *pubsub.Pubsub, logger *logging.Logger, config configData) {
-	d, err := daikinClient.NewNetwork(daikinClient.AddressTokenOption(config.address, config.token))
-	if err != nil {
-		logger.Fatal(fmt.Sprintf("daikin (%s): %v", config.name, err))
-		return
-	}
+	for {
+		time.Sleep(20 * time.Second)
 
-	dev := d.Devices[config.address]
-	if err := dev.GetControlInfo(); err != nil {
-		logger.Fatal(fmt.Sprintf("daikin (%s): %v", config.name, err))
-		return
-	}
+		d, err := daikinClient.NewNetwork(daikinClient.AddressTokenOption(config.address, config.token))
+		if err != nil {
+			logger.Error(fmt.Sprintf("daikin (%s): %v", config.name, err))
+			continue
+		}
 
-	subControl, _ := bus.Subscribe(fmt.Sprintf("daikin.%s.control", config.name))
-	defer subControl.Close()
+		dev := d.Devices[config.address]
+		if err := dev.GetControlInfo(); err != nil {
+			logger.Error(fmt.Sprintf("daikin (%s): %v", config.name, err))
+			continue
+		}
 
-	for event := range subControl.Ch {
-		if event.Key == "power" && event.Value == "off" {
-			if err := dev.GetControlInfo(); err != nil {
-				logger.Error(fmt.Sprintf("daikin (%s): %v", config.name, err))
-				continue
+		subControl, _ := bus.Subscribe(fmt.Sprintf("daikin.%s.control", config.name))
+		defer subControl.Close()
+
+		for event := range subControl.Ch {
+			if event.Key == "power" && event.Value == "off" {
+				if err := dev.GetControlInfo(); err != nil {
+					logger.Error(fmt.Sprintf("daikin (%s): %v", config.name, err))
+					continue
+				}
+
+				dev.ControlInfo.Power = daikinClient.PowerOff
+				if err := dev.SetControlInfo(); err != nil {
+					logger.Error(fmt.Sprintf("daikin (%s): error setting control: %v", config.name, err))
+					continue
+				}
+				logger.Debug(fmt.Sprintf("daikin (%s): power changed to off", config.name))
+			} else if event.Key == "power" && event.Value == "on" {
+				if err := dev.GetControlInfo(); err != nil {
+					logger.Error(fmt.Sprintf("daikin (%s): %v", config.name, err))
+					continue
+				}
+
+				dev.ControlInfo.Power = daikinClient.PowerOn
+				if err := dev.SetControlInfo(); err != nil {
+					logger.Error(fmt.Sprintf("daikin (%s): error setting control: %v", config.name, err))
+					continue
+				}
+				logger.Debug(fmt.Sprintf("daikin (%s): power changed to on", config.name))
+			} else {
+				logger.Error(fmt.Sprintf("daikin (%s): unrecognised event: %v", config.name, event))
 			}
-
-			dev.ControlInfo.Power = daikinClient.PowerOff
-			if err := dev.SetControlInfo(); err != nil {
-				logger.Error(fmt.Sprintf("daikin (%s): error setting control: %v", config.name, err))
-				continue
-			}
-			logger.Debug(fmt.Sprintf("daikin (%s): power changed to off", config.name))
-		} else if event.Key == "power" && event.Value == "on" {
-			if err := dev.GetControlInfo(); err != nil {
-				logger.Error(fmt.Sprintf("daikin (%s): %v", config.name, err))
-				continue
-			}
-
-			dev.ControlInfo.Power = daikinClient.PowerOn
-			if err := dev.SetControlInfo(); err != nil {
-				logger.Error(fmt.Sprintf("daikin (%s): error setting control: %v", config.name, err))
-				continue
-			}
-			logger.Debug(fmt.Sprintf("daikin (%s): power changed to on", config.name))
-		} else {
-			logger.Error(fmt.Sprintf("daikin (%s): unrecognised event: %v", config.name, event))
 		}
 	}
 }
